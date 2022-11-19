e01139b0d69ca2fdd7af704a8987612468c91644
Nikolay <Onedaydejavu124@mail.ru>
2022-11-19 21:21:00 +0700

    Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N

---
+++ EX005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+void Kub (int number)
+{
+    for(int i = 1; i <= number; i++)
+    {
+        int kub = i * i * i;
+        System.Console.WriteLine($"Куб числа {i} = {kub}");
+    }
+}
+
+System.Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine() !);
+
+Kub(number);
+
+
